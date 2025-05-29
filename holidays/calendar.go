2d26fbb69e962434a3361ca5ca8e9d048ba62fc5
Dmytrii Pavliuk <dmitrypavliuk737@email.com>
2025-05-29 13:13:13 +0200

    Add calendar system, update main.go

---
+++ holidays/calendar.go
@@ -0,0 +1,22 @@
+package holidays
+
+import "time"
+
+type Calendar struct{
+	year int
+	holidays []Holiday
+}
+
+func NewCalendar(year int) *Calendar {
+	return &Calendar{
+		year: year,
+		holidays: []Holiday{
+			NewFixedHoliday("Christmas", time.December, 25, year),
+			NewFixedHoliday("Epiphany", time.January, 6, year),
+		}
+	}
+}
+
+func (c *Calendar) GetHolidays() []Holiday {
+	return c.holidays
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	"github.com/DmytriiPavliuk/church-calendar/holidays"
+)
 
 func main() {
-	fmt.Println("Church Holidays Calendar")
+	year := time.Now().Year()
+	calendar := holidays.NewCalendar(year)
+
+	fmt.Println("Church Holidays for %d:\n", year)
+	for _, h := range calendar.GetHolidays() {
+		fmt.Println("%s: %s\n", h.Name(), h.Date().Format("2006-01-02"))
+	}
 }
