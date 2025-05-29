0791c77baefa7345e4459958960931d122048092
Dmytrii Pavliuk <dmitrypavliuk737@email.com>
2025-05-29 16:16:35 +0200

    Fix bug in main.go(Println/Printf)

--- main.go
+++ main.go
@@ -11,8 +11,8 @@ func main() {
 	year := time.Now().Year()
 	calendar := holidays.NewCalendar(year)
 
-	fmt.Println("Church Holidays for %d:\n", year)
+	fmt.Printf("Church Holidays for %d:\n", year)
 	for _, h := range calendar.GetHolidays() {
-		fmt.Println("%s: %s\n", h.Name(), h.Date().Format("2006-01-02"))
+		fmt.Printf("%s: %s\n", h.Name(), h.Date().Format("2006-01-02"))
 	}
 }
